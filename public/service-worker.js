f8c15954ec7d4e59aed60b065fa74a2ad1a97a15
Salvo Dragotta <salvo.dragotta@mottmac.com>
2021-10-10 20:40:24 +1100

    Add extra icons

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,10 +1,10 @@
 const FILES_TO_CACHE = [    
     '/',
     '/index.html',
-    '/style.css',
-    "/index.js",    
-    "/icons/icon-192x192.png",
-    "/icons/icon-512x512.png"   
+    '/styles.css',
+    '/index.js',    
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png'   
   ];
 
 
